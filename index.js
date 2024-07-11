e285785425e1d01a1e601cb5a59f949f576e6e79
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 14:02:23 +0300

    first class functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const drivers=['Antonia','Nuru','Amari','Mo']
+
+const returnFirstTwoDrivers = () => {
+  return drivers.slice(0,2)
+} 
+
+
+const returnLastTwoDrivers = () => {
+  return drivers.slice(-2)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+  return (fare) => {
+    return fare * multiplier;
+  };
+};
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers,func) => {
+  return  func(arrayOfDrivers)
+  
+ 
+}
+console.log()
